c0e9a21b763443db35c6edecba1c8303a925f807
David Grayson <davidegrayson@gmail.com>
2018-11-09 09:09:36 -0800

    Finished the API and started implementing create().

--- tapi.cpp
+++ tapi.cpp
@@ -1,5 +1,69 @@
 #include "tapi.h"
+#include "yaml.h"
 
-unsigned tapi::APIVersion::getMajor() noexcept {
+using namespace tapi;
+
+unsigned APIVersion::getMajor() noexcept {
   return 2;  // 2.0.0
+}
+
+LinkerInterfaceFile * LinkerInterfaceFile::create(const std::string & path,
+  const uint8_t * data, size_t size,
+  cpu_type_t cpuType, cpu_subtype_t cpuSubType,
+  CpuSubTypeMatching matchingMode, PackedVersion32 minOSVersion,
+  std::string & error) noexcept {
+
+  error.clear();
+
+  if (path.empty()) {
+    error = "The path argument is empty.";
+    return NULL;
+  }
+
+  if (data == NULL) {
+    error = "The data pointer is null.";
+    return NULL;
+  }
+
+  if (strcmp(data, "---"))
+  {
+    error = "Data does not start with three dashes; might be a binary.";
+    return NULL;
+  }
+
+  yaml_parser_t parser;
+  if (!error.size()) {
+    int success = yaml_parser_initialize(&parser);
+    if (!success) {
+      error = "Failed to initialize YAML parser.";
+    }
+  }
+
+  yaml_document_t doc;
+  if (!error.size()) {
+    yaml_parser_set_input_string(&parser, data, size);
+    int success = yaml_parser_load(&parser, &doc);
+    if (!success) {
+      error = "Failed to initialize YAML parser.";
+    }
+  }
+
+  LinkerInterfaceFile * file = NULL;
+  if (!error.size()) {
+    file = new LinkerInterfaceFile();
+  }
+
+  // ORIG: set patch level to 0 on minOSVersion
+
+  error = "Parsing unimplemented!";  // TODO
+
+  yaml_parser_delete(&parser);
+  yaml_document_delete(&doc);
+
+  if (error.size() && file) {
+    delete file;
+    file = NULL;
+  }
+
+  return file;
 }
--- tapi.h
+++ tapi.h
@@ -24,27 +24,29 @@ class PackedVersion32 {
 public:
   PackedVersion32() = default;
   PackedVersion32(uint32_t v) : version(v) {}
+  operator unsigned() const noexcept { return version; }
 };
 
-enum class ObjCConstraint {
-  None,
-  Retain_Release,
-  Retain_Release_For_Simulator,
-  Retain_Release_Or_GC,
-  GC,
+enum class ObjCConstraint : unsigned {
+  None = 0,
+  Retain_Release = 1,
+  Retain_Release_For_Simulator = 2,
+  Retain_Release_Or_GC = 3,
+  GC = 4,
 };
 
-enum class Platform {
-  Unknown,
-  OSX,
-  iOS,
-  watchOS,
-  tvOS
+enum class Platform : unsigned  {
+  Unknown = 0,
+  OSX = 1,
+  iOS = 2,
+  watchOS = 3,
+  tvOS = 4,
+  bridgeOS = 5,
 };
 
-enum class CpuSubTypeMatching {
-  Exact,
-  ABI_Compatible
+enum class CpuSubTypeMatching : unsigned {
+  ABI_Compatible = 0,
+  Exact = 1,
 };
 
 class Symbol {
@@ -56,7 +58,16 @@ public:
 };
 
 class LinkerInterfaceFile {
+  LinkerInterfaceFile() = default;
 public:
+  LinkerInterfaceFile(const LinkerInterfaceFile &) noexcept = delete;
+  LinkerInterfaceFile & operator=(const LinkerInterfaceFile &) noexcept = delete;
+
+  LinkerInterfaceFile(LinkerInterfaceFile &&) noexcept;
+  LinkerInterfaceFile & operator=(LinkerInterfaceFile &&) noexcept;
+
+  ~LinkerInterfaceFile() noexcept;
+
   static LinkerInterfaceFile * create(const std::string & path,
     const uint8_t * data, size_t size,
     cpu_type_t cpuType, cpu_subtype_t cpuSubType,
@@ -72,7 +83,30 @@ public:
   static bool areEquivalent(const std::string & tbdPath,
     const std::string & dylibPath) noexcept;
 
+  const std::string & getInstallName() const noexcept;
+  bool isInstallNameVersionSpecific() const noexcept;
+  Platform getPlatform() const noexcept;
+  PackedVersion32 getCurrentVersion() const noexcept;
+  PackedVersion32 getCompatibilityVersion() const noexcept;
+  unsigned getSwiftVersion() const noexcept;
+  ObjCConstraint getObjCConstraint() const noexcept;
+  const std::string & getParentFrameworkName() const noexcept;
+  bool isApplicationExtensionSafe() const noexcept;
+  bool hasTwoLevelNamespace() const noexcept;
+
+  bool hasAllowableClients() const noexcept;
+  const std::vector<std::string> & allowableClients() const noexcept;
+
+  bool hasReexportedLibraries() const noexcept;
+  const std::vector<std::string> & reexportedLibraries() const noexcept;
+
+  bool hasWeakDefinedExports() const noexcept;
+
+  const std::vector<std::string> & ignoreExports() const noexcept;
+
   const std::vector<Symbol> & exports() const noexcept;
+
+  const std::vector<Symbol> & undefineds() const noexcept;
 };
 
 }  // end namespace tapi
